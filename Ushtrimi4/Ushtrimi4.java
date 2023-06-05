e43b8c39460dbd560b9457c615f0d52121608ad5
rrezonk16 <rrezonkrasniqi32@gmail.com>
2023-06-06 01:11:51 +0200

    Detyra e 4

--- Ushtrimi4/Ushtrimi4.java
+++ Ushtrimi4/Ushtrimi4.java
@@ -1,7 +1,29 @@
-public class Ushtrimi4 {
+import java.util.Scanner;
 
+public class Ushtrimi4 {
     public static void main(String[] args) {
-        
-        System.out.println("Ur mom");
+        Scanner input = new Scanner(System.in);
+        int numri;
+        int numratTotal = 0;
+        int shumefishet = 0;
+
+        System.out.println("Shtypni numrat e plote (terminohet me -9999):");
+
+        while (true) {
+            numri = input.nextInt();
+            
+            if (numri == -9999) {
+                break;
+            }
+            
+            numratTotal++;
+
+            if (numri % 4 == 0 && numri % 8 == 0) {
+                shumefishet++;
+            }
+        }
+
+        System.out.println("Numrat e shtypur gjithsej: " + numratTotal);
+        System.out.println("Numrat shumefish të 4 dhe 8: " + shumefishet);
     }
 }
